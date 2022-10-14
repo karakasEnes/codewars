3d39d97c5106ca5c0995f82e60609eed361f7406
Enes Karakas <enes.coding@gmail.com>
2022-10-14 18:20:28 +0300

    kata added

---
+++ 6kyu/javaScript/replaceWithAlphabetPosition.js
@@ -0,0 +1,76 @@
+/*
+
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+*/
+
+function alphabetPosition(text) {
+  return text
+    .split('')
+    .filter((n) => n.toLowerCase() !== n.toUpperCase())
+    .map((letter) => parseInt(letter, 36) - 9)
+    .join(' ');
+}
+
+function alphabetPosition(text) {
+  var result = '';
+  for (var i = 0; i < text.length; i++) {
+    var code = text.toUpperCase().charCodeAt(i);
+    if (code > 64 && code < 91) result += code - 64 + ' ';
+  }
+
+  return result.slice(0, result.length - 1);
+}
+
+let alphabetPosition = (text) =>
+  text
+    .toUpperCase()
+    .replace(/[^A-Z]/g, '')
+    .split('')
+    .map((ch) => ch.charCodeAt(0) - 64)
+    .join(' ');
+
+function alphabetPosition(text) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text
+    .toLowerCase()
+    .split('')
+    .filter((letter) => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    })
+    .map((letter) => alphabet.indexOf(letter) + 1)
+    .join(' ');
+}
+
+function alphabetPosition(text) {
+  return text
+    .toLowerCase()
+    .split('')
+    .map((a) => a.charCodeAt(0) - 96)
+    .filter((a) => a > 0 && a < 27)
+    .join(' ');
+}
+
+function alphabetPosition(text) {
+  text = text
+    .replace(/([^a-z])/gi, '')
+    .toLowerCase()
+    .split('');
+
+  for (let i = 0; i < text.length; i++) {
+    text[i] = text[i].charCodeAt() - 96;
+  }
+  return text.join(' ');
+}
